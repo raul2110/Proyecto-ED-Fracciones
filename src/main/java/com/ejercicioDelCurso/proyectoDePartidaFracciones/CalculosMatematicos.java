9d87e102a1ef9241767c5bfcc1d3be9183554772
raul2110 <raul2110dm@gmail.com>
2020-05-05 13:46:30 +0200

    pruebas mcm

--- src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/CalculosMatematicos.java
+++ src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/CalculosMatematicos.java
@@ -1,4 +1,4 @@
-package com.ejercicioDelCurso.proyectoDePartidaFracciones;
+package main.java.com.ejercicioDelCurso.proyectoDePartidaFracciones;
 
 import java.util.ArrayList;
 import java.util.SortedMap;
--- src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/Fraccion.java
+++ src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/Fraccion.java
@@ -1,4 +1,4 @@
-package com.ejercicioDelCurso.proyectoDePartidaFracciones;
+package main.java.com.ejercicioDelCurso.proyectoDePartidaFracciones;
 
 
 
