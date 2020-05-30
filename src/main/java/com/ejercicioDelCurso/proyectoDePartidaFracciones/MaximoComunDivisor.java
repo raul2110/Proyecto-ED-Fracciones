a8e250a548c714852741a4cc6fb32d7200b1b327
Daniel Suarez <bemol54192@gmail.com>
2020-05-30 13:31:22 +0200

    Añadida clase MaximoComunDivisor

--- src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/MaximoComunDivisor.java
+++ src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/MaximoComunDivisor.java
@@ -1,9 +1,49 @@
 package main.java.com.ejercicioDelCurso.proyectoDePartidaFracciones;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
 public class MaximoComunDivisor {
 
-	void Prueba() {
-		//maximo comun divisor
+	public static int GetMaximoComunDivisor(int n1, int n2) {
+		
+		ArrayList<Integer> divisoresN1 = CalculosMatematicos.divisoresTotales(n1);
+		ArrayList<Integer> divisoresN2 = CalculosMatematicos.divisoresTotales(n2);
+			
+		for(int i=0; i<divisoresN1.size(); i++) {
+			if(divisoresN2.contains(divisoresN1.get(i))) {
+				return divisoresN1.get(i);
+			}
+		}
+		
+		return 1;
+	}
+	
+	public static int GetMaximoComunDivisor(ArrayList<Integer> nums) {
+		
+		ArrayList<Integer> divisores = new ArrayList<>();
+		
+		Collections.sort(nums);
+		
+		divisores.addAll(CalculosMatematicos.divisoresTotales(nums.get(0)));
+		
+		int valid = 0;
+		
+		for(Integer d:divisores) {
+			for(Integer n:nums) {
+				if(n%d == 0) {
+					valid++;
+				}
+			}
+			
+			if(valid == nums.size()) {
+				return d;
+			}
+			valid = 0;
+		}
+		
+		return 1;
 	}
 	
 }
