ad09f8927ef1100b3e14a6684cd10809cdc727a1
Daniel Suarez <bemol54192@gmail.com>
2020-05-30 15:44:28 +0200

    Añadida clase ObjectOutputStreamHeaderLess

---
+++ src/main/java/com/ejercicioDelCurso/proyectoDePartidaFracciones/ObjectOutputStreamHeaderLess.java
@@ -0,0 +1,17 @@
+package objects;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class ObjectOutputStreamHeaderLess extends ObjectOutputStream{
+
+	public ObjectOutputStreamHeaderLess(OutputStream out) throws IOException {
+		super(out);
+	}
+		
+	public void writeStreamHeader() {
+			
+	}
+		
+}
